21ee8c4121032f32a85d2ddcb3fad0b71f0b7e65
Marek Kosinski <kosmarek11@gmail.com>
2022-10-14 16:38:10 +0200

    gee test v4 properly deserialized

--- AntiCaptchaApi.Net/Models/Solutions/GeeTestV4Solution.cs
+++ AntiCaptchaApi.Net/Models/Solutions/GeeTestV4Solution.cs
@@ -1,11 +1,18 @@
-﻿namespace AntiCaptchaApi.Net.Models.Solutions;
+﻿using Newtonsoft.Json;
+
+namespace AntiCaptchaApi.Net.Models.Solutions;
 
 public class GeeTestV4Solution : BaseSolution
 {
+    [JsonProperty("captcha_id")]
     public string CaptchaId { get; internal set; }
+    [JsonProperty("lot_number")]
     public string LotNumber { get; internal set; }
+    [JsonProperty("pass_token")]
     public string PassToken { get; internal set; }
+    [JsonProperty("gen_time")]
     public string GenTime { get; internal set; }
+    [JsonProperty("captcha_output")]
     public string CaptchaOutput { get; internal set; }
 
     public override bool IsValid() =>
